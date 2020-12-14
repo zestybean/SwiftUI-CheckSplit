a582cfe38d3497d9621e11e120cb53d3d49fafd1
Brando Lugo <brando.lugo@gmail.com>
2020-12-14 13:32:24 -0600

    Initial Commit

---
+++ Shared/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
