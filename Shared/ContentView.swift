28cb29017ca7383fc5a8a182379f82ebd6aba02b
Brando Lugo <brando.lugo@gmail.com>
2020-12-15 13:34:53 -0600

    Fixed a bug where adding a on tap feature to the form caused the keyboard to be dismissed but the rest of the controllers would bug out

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -17,27 +17,40 @@ struct ContentView: View {
     
     let tipPercentages = [0, 10, 15, 20, 25]
     
-    var totalPerPerson: Double {
-        //Calc the total per person here
-        let peopleCount = Double(numberOfPeople + 2)
+    var grandTotal: Double {
+        //Calc the grand total here
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
         
-        
         let tipValue = orderAmount / 100 * tipSelection
         let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double {
+        //Calc the total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        
         let amountPerPerson = grandTotal / peopleCount
         
         return amountPerPerson
     }
     
     var body: some View {
-        Background {
+
             NavigationView {
                     Form {
                         Section {
-                            TextField("Enter Check Amount", text: $checkAmount)
-                                .keyboardType(.decimalPad)
+                            HStack {
+                                TextField("Enter Check Amount", text: $checkAmount)
+                                    .keyboardType(.decimalPad)
+                                
+                                Button("Submit") {
+                                    self.hideKeyboard()
+                                }
+                            }
+                            
                             
                             Picker("Number of people", selection: $numberOfPeople) {
                                 ForEach(2..<100){
@@ -46,7 +59,7 @@ struct ContentView: View {
                             }
                         }
                         
-                        Section(header: Text("Please choose a tip amount")) {
+                        Section(header: Text("Slide to choose a tip amount")) {
                             Picker("Tip percentage", selection: $tipPercentage) {
                                 ForEach(0..<tipPercentages.count) {
                                     Text("\(self.tipPercentages[$0])%")
@@ -55,43 +68,26 @@ struct ContentView: View {
                             .pickerStyle(SegmentedPickerStyle())
                         }
                         
+                        Section(header: Text("Grand total with tip")) {
+                            Text("$\(grandTotal, specifier: "%.2f")")
+                        }
+                        
                         Section(header: Text("Total amount per person")) {
                             Text("$\(totalPerPerson, specifier: "%.2f")")
                         }
                     }
-                    .navigationTitle("WeSplit")
+                    .navigationTitle("WeSplit ☇")
             }
-        }  .onTapGesture {
-            self.endEditing()
-        }
-      
-        
-    }
-    
-    private func endEditing() {
-        UIApplication.shared.endEditing()
     }
 }
 
-struct Background<Content: View>: View {
-    private var content: Content
-
-    init(@ViewBuilder content: @escaping () -> Content) {
-        self.content = content()
-    }
-
-    var body: some View {
-        Color.white
-        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-        .overlay(content)
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
-
-extension UIApplication {
-    func endEditing() {
-        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
-}
+#endif
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
